2736f506def215f0e3508838d45b5c1c90ceb6eb
Rodney Ruswa <rodney.ruswa@bjss.com>
2017-12-18 14:02:03 +0000

    Add a Number class with methods for generating random numbers

---
+++ src/main/java/activesupport/number/Number.java
@@ -0,0 +1,24 @@
+package activesupport.number;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Number {
+    /**
+     * Returns a number between 1 and the parameter max which is passed in as an argument.
+     *
+     * @param max an integer specifying the maximum value which is inclusive.
+     * */
+    public static int random(int max){
+        return random(1, max);
+    }
+
+    /**
+     * Returns a number between the min and max which are passed in as arguments.
+     *
+     * @param min an integer specifying the minimum value, which is inclusive.
+     * @param max an integer specifying the maximum value, which is inclusive.
+     * */
+    public static int random(int min, int max){
+        return ThreadLocalRandom.current().nextInt(min, max + 1);
+    }
+}
