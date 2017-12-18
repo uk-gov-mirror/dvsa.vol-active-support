58622255e3f3775f9043c6d88c65b047fda8c7a3
Rodney Ruswa <rodney.ruswa@bjss.com>
2017-12-18 14:56:02 +0000

    Change Bool from being package private to public

--- src/main/java/activesupport/bool/Bool.java
+++ src/main/java/activesupport/bool/Bool.java
@@ -2,7 +2,7 @@ package activesupport.bool;
 
 import activesupport.number.Int;
 
-class Bool{
+public class Bool{
     public static boolean random(){
         return (Int.random(0, 1) == 1) ? true : false;
     }
