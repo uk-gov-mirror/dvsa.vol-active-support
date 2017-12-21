ba8668e5dedb2ec50a7147e7b295db4752db66a2
Rodney Ruswa <rodney.ruswa@bjss.com>
2017-12-21 10:18:33 +0000

    Add Util#loadConfigProperties and Util#loadProperties to handle adding properties from files into system properties

--- src/main/java/activesupport/aws/s3/util/Util.java
+++ src/main/java/activesupport/aws/s3/util/Util.java
@@ -1,7 +1,11 @@
 package activesupport.aws.s3.util;
 
+import activesupport.system.Properties;
 import activesupport.system.out.Output;
 import org.jetbrains.annotations.NotNull;
+
+import java.io.FileNotFoundException;
+import java.nio.file.Paths;
 
 public class Util {
 
@@ -20,6 +24,30 @@ public class Util {
         }
 
         return String.format("olcs.%s.nonprod.dvsa.aws/email/%s", env, S3ObjectName);
+    }
+
+    /**
+     * Adds to system properties the properties specified at properties/config.properties
+     * */
+    public static void loadConfigProperties(){
+        String pathToProperties = "properties/config.properties";
+        loadProperties(pathToProperties);
+    }
+
+    /**
+     * Adds the properties of the property file at the specified path to system properties.
+     * @param path path to a properties file from project root.
+     * */
+    public static void loadProperties(@NotNull String path){
+        // Adds properties specified in properties/config.properties into system properties
+        if(java.nio.file.Files.exists(Paths.get("properties/config.properties"))) {
+            try {
+                Properties.loadConfigPropertiesFromFile();
+            } catch (FileNotFoundException e) {
+                System.out.println(Output.printColoredLog("[WARNING] PROPERTIES from path: " + path + " not added to system properties"));
+                e.printStackTrace();
+            }
+        }
     }
 
 }
