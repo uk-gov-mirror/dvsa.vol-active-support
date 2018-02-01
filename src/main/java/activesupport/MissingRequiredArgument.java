cd5c26f211ca6e2abf8f86754459adee71daaaa7
Rodney Ruswa <rodney.ruswa@bjss.com>
2018-02-01 12:03:57 +0000

    Create custom exception to notify callers that they are missing a required property

---
+++ src/main/java/activesupport/MissingRequiredArgument.java
@@ -0,0 +1,21 @@
+package activesupport;
+
+import activesupport.system.out.Output;
+
+public class MissingRequiredArgument extends Exception {
+    public MissingRequiredArgument() {
+        super("[ERROR] You are missing an argument that needs to be specified at run time or in your system variables");
+    }
+
+    public MissingRequiredArgument(String message) {
+        super(Output.printColoredLog(message));
+    }
+
+    public MissingRequiredArgument(String message, Throwable cause) {
+        super(Output.printColoredLog(message), cause);
+    }
+
+    public MissingRequiredArgument(Throwable cause) {
+        super(cause);
+    }
+}
