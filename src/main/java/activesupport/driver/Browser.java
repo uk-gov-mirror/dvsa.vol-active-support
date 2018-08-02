160a7b0835d7ad87c74ebea26e27e443d530e393
Kishan Patel <kishan.patel@dvsa.gov.uk>
2018-08-02 15:37:26 +0100

    refactoring browser method, so the browser would reopen after quit is called within the tests

--- src/main/java/activesupport/driver/Browser.java
+++ src/main/java/activesupport/driver/Browser.java
@@ -5,6 +5,7 @@ import activesupport.MissingDriverException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxBinary;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 
@@ -17,52 +18,18 @@ public class Browser {
     private String getLocalChromeDriver = System.getenv("CHROMEDRIVER");
     private String getLocalGeckoDriver = System.getenv("FIREFOXDRIVER");
     private static WebDriver driver;
-    private static String browserName = System.getProperty("browser");
-
-    public void selectBrowser() throws IllegalBrowserException, MissingDriverException {
-        String windows = ".exe";
-        String chrome = "chromedriver";
-        String firefoxDriver = "geckodriver";
-        try {
-            if (operatingSystem.contains("Windows") && browserName.contains("chrome")) {
-                checkFileExists(windows, chrome, "webdriver.chrome.driver");
-            }
-            if (!operatingSystem.contains("Windows") && !browserName.contains("chrome")) {
-                checkFileExists(windows, firefoxDriver, "webdriver.firefox.marionette");
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-            throw new MissingDriverException();
-        }
-    }
-
-    public WebDriver navigate() throws IllegalBrowserException {
-      driver =  whichBrowser(browserName);
-      setDriver(driver);
-      return driver;
-    }
+    private  String browserName = System.getProperty("browser");
 
     public static void setDriver(WebDriver driver) {
         Browser.driver = driver;
     }
 
-    private void checkFileExists(String windows, String browserName, String driverProperty) {
-        File file = new File(getCurrentWorkingDirectory + browserName + windows);
-        if (file.exists() &&(browserName.equals("chrome")))
-        {
-            createDriverPath(driverProperty, getCurrentWorkingDirectory, browserName, windows);
-        } else {
-            createDriverPath(driverProperty, getLocalChromeDriver, null, null);
-        }
-        if (file.exists() &&(browserName.equals("firefox"))) {
-            createDriverPath(driverProperty, getCurrentWorkingDirectory, browserName, windows);
-        } else {
-            createDriverPath(driverProperty, getLocalGeckoDriver, null, null);
-        }
-    }
+    public static WebDriver navigate() throws IllegalBrowserException {
 
-    private static String createDriverPath(String driverProperty, String path, String extention, String extention2){
-      return  System.setProperty(driverProperty, path + extention + extention2);
+        if (driver == null) {
+               setDriver(whichBrowser(System.getProperty("browser")));
+           }
+           return driver;
     }
 
     private static WebDriver whichBrowser(String browserName) throws IllegalBrowserException {
@@ -70,10 +37,14 @@ public class Browser {
 
         switch (browserName) {
             case "headless":
-                ChromeOptions optionsChrome = new ChromeOptions();
-                optionsChrome.setHeadless(true);
+                FirefoxOptions options = new FirefoxOptions();
+               options.setHeadless(true);
+                options.setCapability("javascriptEnabled", true);
+                options.setCapability("handleAlerts", true);
+                options.setCapability("marionette", true);
                 if (driver == null)
-                    driver = new ChromeDriver(optionsChrome);
+                    driver = new FirefoxDriver(options);
+
                 break;
             case "chrome":
                 if (driver == null)
@@ -81,7 +52,7 @@ public class Browser {
                 break;
             case "firefox":
                 FirefoxOptions optionsFirefox = new FirefoxOptions();
-               optionsFirefox.setCapability("marionette", false);
+               optionsFirefox.setCapability("marionette", true);
                 if (driver == null)
                     driver = new FirefoxDriver(optionsFirefox);
                 break;
@@ -91,8 +62,20 @@ public class Browser {
         return driver;
     }
 
-    public void quit() throws IllegalBrowserException, MissingDriverException {
-        if (navigate() != null)
-            navigate().quit();
+    public static void quit() throws IllegalBrowserException, MissingDriverException {
+        if (driver != null)
+           driver.close();
+           setDriver(null);
+    }
+
+    public static   boolean isBrowserOpen() throws IllegalBrowserException {
+        boolean isClosed = false;
+
+            if (driver != null) {
+                isClosed = true;
+            } else {
+            isClosed = false;
+        }
+        return isClosed;
     }
 }
