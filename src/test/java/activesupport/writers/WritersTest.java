f945cec3e832090631e37eeb571b429f5c09715d
Kishan Patel <kishan.patel@dvsa.gov.uk>
2018-08-13 16:59:55 +0100

    updating

---
+++ src/test/java/activesupport/writers/WritersTest.java
@@ -0,0 +1,16 @@
+package activesupport.writers;
+
+//import activesupport.enums.FileType;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.security.PublicKey;
+
+public class WritersTest {
+
+
+    public static void main(String[] args) {
+        System.out.println(System.getProperty("os.name"));
+    }
+
+}
