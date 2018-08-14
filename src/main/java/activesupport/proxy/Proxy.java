8f90b041333af83ccd57c72e7b9ac115dde9174e
Kishan Patel <kishan.patel@dvsa.gov.uk>
2018-08-14 12:13:58 +0100

    adding proxy method to active support

--- src/main/java/activesupport/proxy/Proxy.java
+++ src/main/java/activesupport/proxy/Proxy.java
@@ -1,4 +1,14 @@
-package activesupport.security;
+package activesupport.proxy;
 
 public class Proxy {
+
+    private static org.openqa.selenium.Proxy createZapProxyConfigurationForWebDriver(String iPAddress, String portNo) {
+        org.openqa.selenium.Proxy proxy = new org.openqa.selenium.Proxy();
+
+        proxy.setHttpProxy(String.format("%s:%s", iPAddress, portNo));
+        proxy.setFtpProxy(String.format("%s:%s", iPAddress, portNo));
+        proxy.setSslProxy(String.format("%s:%s", iPAddress, portNo));
+        return proxy;
+    }
+
 }
