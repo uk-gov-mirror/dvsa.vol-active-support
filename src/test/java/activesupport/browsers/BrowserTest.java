dd941d87304da6c63bb1a3affc546f3a157ba869
Kishan Patel <kishan.patel@dvsa.gov.uk>
2018-08-28 11:57:00 +0100

    creating units of tests per browser

--- src/test/java/activesupport/browsers/BrowserTest.java
+++ src/test/java/activesupport/browsers/BrowserTest.java
@@ -3,6 +3,7 @@ package activesupport.browsers;
 import activesupport.IllegalBrowserException;
 import activesupport.MissingDriverException;
 import activesupport.driver.Browser;
+import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
@@ -10,14 +11,55 @@ import static org.junit.Assert.assertTrue;
 
 public class BrowserTest {
 
+@Test
+    public void chromeTest() throws MissingDriverException, IllegalBrowserException {
 
-    public static void main(String[] args) throws MissingDriverException, IllegalBrowserException, InterruptedException {
+             System.setProperty("browser", "chrome");
+            Browser.navigate().get("http://bbc.co.uk");
+            if (Browser.isBrowserOpen()) {
+                Browser.quit();
+            }
+            Browser.navigate().get("http://www.google.co.uk");
 
-        System.setProperty("browser","firefox");
+        }
+
+    @Test
+    public void firefoxTest() throws MissingDriverException, IllegalBrowserException {
+
+        System.setProperty("browser", "firefox");
         Browser.navigate().get("http://bbc.co.uk");
         if (Browser.isBrowserOpen()) {
             Browser.quit();
         }
         Browser.navigate().get("http://www.google.co.uk");
+
+    }
+
+    @Test
+    public void proxyTest() throws MissingDriverException, IllegalBrowserException {
+
+        System.setProperty("browser", "proxy");
+        Browser.navigate().get("http://bbc.co.uk");
+        if (Browser.isBrowserOpen()) {
+            Browser.quit();
         }
+        Browser.navigate().get("http://www.google.co.uk");
+
+    }
+
+    @Test
+    public void headlessTest() throws MissingDriverException, IllegalBrowserException {
+
+        System.setProperty("browser", "headless");
+        Browser.navigate().get("http://bbc.co.uk");
+        if (Browser.isBrowserOpen()) {
+            Browser.quit();
+        }
+        Browser.navigate().get("http://www.google.co.uk");
+
+    }
+
+
 }
+
+
