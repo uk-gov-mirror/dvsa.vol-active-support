c093419bcb98868912e83848b28b4763a8654922
Rodney Ruswa <rodney.ruswa@bjss.com>
2018-11-22 11:20:38 +0000

    Create MailClientUtils#getText which will return the body of a multipart message as a string

---
+++ src/main/java/activesupport/mail/util/MailClientUtils.java
@@ -0,0 +1,52 @@
+package activesupport.mail.util;
+
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.Part;
+import java.io.IOException;
+
+public class MailClientUtils {
+
+    /**
+     * Return the primary text content of the message.
+     */
+    public static String getText(Part p) throws MessagingException, IOException {
+        boolean textIsHtml = false;
+        if (p.isMimeType("text/*")) {
+            String s = (String)p.getContent();
+            textIsHtml = p.isMimeType("text/html");
+            return s;
+        }
+
+        if (p.isMimeType("multipart/alternative")) {
+            // prefer html text over plain text
+            Multipart mp = (Multipart)p.getContent();
+            String text = null;
+            for (int i = 0; i < mp.getCount(); i++) {
+                Part bp = mp.getBodyPart(i);
+                if (bp.isMimeType("text/plain")) {
+                    if (text == null)
+                        text = getText(bp);
+                    continue;
+                } else if (bp.isMimeType("text/html")) {
+                    String s = getText(bp);
+                    if (s != null)
+                        return s;
+                } else {
+                    return getText(bp);
+                }
+            }
+            return text;
+        } else if (p.isMimeType("multipart/*")) {
+            Multipart mp = (Multipart)p.getContent();
+            for (int i = 0; i < mp.getCount(); i++) {
+                String s = getText(mp.getBodyPart(i));
+                if (s != null)
+                    return s;
+            }
+        }
+
+        return null;
+    }
+
+}
